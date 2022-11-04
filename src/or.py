7947de8c587084d8401801ca5b6f28cb18f3b692
yuto-yuto <yuto.nakatsu@gmail.com>
2022-11-04 13:32:48 +0100

    Add or

---
+++ src/or.py
@@ -0,0 +1,15 @@
+print("---- or ----")
+print(None or 1)
+print(0 or 1)
+print(32 or 1)
+print("" or 1)
+print("something" or 1)
+print(() or 1)
+print((22) or 1)
+print([] or 1)
+print([1] or 1)
+
+print("---- and ----")
+print(3 & 1) # 11 & 01
+print(1 & 2) # 01 & 10
+print(1 & 4) # 01 & 100
