b89c4b49215b883fdcb7dc8cee4c156a189cd237
yuto-yuto <yuto.nakatsu@gmail.com>
2022-12-15 13:51:27 +0100

    add tuple

---
+++ src/tuple.py
@@ -0,0 +1,10 @@
+data = [
+    ("key1", "value1"),
+    ("key2", "value2"),
+    ("key3", "value3"),
+    ("key4", "value4"),
+]
+
+for key, value in data:
+    print(f"key: {key}, value: {value}")
+
