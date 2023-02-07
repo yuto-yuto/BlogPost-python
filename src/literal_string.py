ed012ccb01ca9e19a316910b50369b07e8601fbc
yuto-yuto <yuto.nakatsu@gmail.com>
2023-02-07 14:14:14 +0000

    Update literal string

--- src/literal_string.py
+++ src/literal_string.py
@@ -1,9 +1,9 @@
-from typing import Literal
+from typing import Dict, get_args, Literal
 from enum import Enum
 
 roles_str = ["manager", "developer", "tester"]
 
-role_ids_with_str: dict[str, int] = {
+role_ids_with_str: Dict[str, int] = {
     "manager": 1,
     "developer": 2,
     "tester": 3,
@@ -27,7 +27,7 @@ class Role(Enum):
     Tester = "tester"
 
 
-role_ids_with_enum: dict[Role, int] = {
+role_ids_with_enum: Dict[Role, int] = {
     Role.Manager: 1,
     Role.Developer: 2,
     Role.Tester: 3,
@@ -35,19 +35,25 @@ role_ids_with_enum: dict[Role, int] = {
 
 
 def get_role_id_with_enum(role: Role):
-    print(f"ID ({role}): {role_ids_with_enum[role]}")
+    print(f"ID ({role.value}): {role_ids_with_enum[role]}")
+    print(f"role: ({role}), name: {role.name}, value: {role.value}")
 
 
 get_role_id_with_enum(Role.Manager)
-# Argument of type "Literal['owner']" cannot be assigned to parameter "role" of type "Role" in function "get_role_id_with_enum"
-# "Literal['owner']" is incompatible with "Role"
-# get_role_id_with_enum("owner")
+# Argument of type "Literal['developer']" cannot be assigned to parameter "role" of type "Role" in function "get_role_id_with_enum"
+# "Literal['developer']" is incompatible with "Role"
+# get_role_id_with_enum("developer")
 
-roles_literal = Literal["manager", "developer", "tester"]
+RolesLiteral = Literal["manager", "developer", "tester"]
+role_ids_with_literal: Dict[RolesLiteral, int] = {
+    "manager": 1,
+    "developer": 2,
+    "tester": 3,
+}
 
 
-def get_role_id_with_literal(role: roles_literal):
-    print(f"ID ({role}): {role_ids_with_str[role]}")
+def get_role_id_with_literal(role: RolesLiteral):
+    print(f"ID ({role}): {role_ids_with_literal[role]}")
 
 
 get_role_id_with_literal("manager")
@@ -58,3 +64,15 @@ get_role_id_with_literal("manager")
 #     "Literal['owner']" cannot be assigned to type "Literal['tester']"
 # get_role_id_with_literal("owner")
 
+
+def get_literal_values():
+    values = get_args(RolesLiteral)
+    print(values)  # ('manager', 'developer', 'tester')
+    dynamic_ids = {}
+    for index, x in enumerate(values):
+        dynamic_ids[x] = index + 1
+
+    print(dynamic_ids) # {'manager': 1, 'developer': 2, 'tester': 3}
+
+
+get_literal_values()
