07be3b90eda82bf675c6b9df8bfe93200a455a8c
yuto-yuto <yuto.nakatsu@gmail.com>
2023-03-01 15:42:53 +0000

    Update get set attr

--- src/get_set_attr.py
+++ src/get_set_attr.py
@@ -5,7 +5,7 @@ class Product:
     def __init__(self):
         self.count: int = 0
         self.name: str = ""
-        self.price: int = 0
+        self.price: float = 0
         self.remark: str = ""
 
 
@@ -56,11 +56,29 @@ def run2():
             f"name: {item.name}, count: {item.count}, price: {item.price}, remark: {item.remark}"
         )
 
+
 def get_set_test():
     item = Product()
-    print(item.__getattribute__("count")) # 0
-    print(getattr(item, "count")) # 0
+    item.count = 11
+    print(item.__getattribute__("count"))  # 11
+    print(getattr(item, "count"))  # 11
+    print(item.count)  # 11
+
+    item.count = 100
+    print(item.count)  # 100
+    item.__setattr__("count", 66)
+    print(item.count)  # 66
     setattr(item, "count", 12)
-    print(item.count) # 12
-    item.__setattr__("count",66)
-    print(item.count) # 66
+    print(item.count)  # 12
+    setattr(item, "unknown_prop", "this is value")
+    print(item.unknown_prop)  # this is value
+    print(getattr(item, "unknown_prop"))  # this is value
+
+    print(hasattr(item, "not_exist_prop"))
+    print(hasattr(item, "count"))
+
+
+get_set_test()
+print()
+# run1()
+run2()
