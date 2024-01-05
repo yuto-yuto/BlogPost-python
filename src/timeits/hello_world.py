fe359910aa91d30ddb2261c97ffbc0a18b19c2d0
yuto-yuto <yuto.nakatsu@gmail.com>
2024-01-05 07:29:19 +0000

    timeit

---
+++ src/timeits/hello_world.py
@@ -0,0 +1,7 @@
+print("Hello")
+
+def hello():
+    print("hello world")
+
+if __name__ == '__main__':
+    print("World!")
---
+++ src/timeits/use_timeit.py
@@ -0,0 +1,44 @@
+import timeit
+from pathlib import Path
+from random import randrange
+
+def func1():
+    return 1
+
+
+def func2(a: int, b: int):
+    return a + b
+
+
+print(timeit.timeit(func1))
+print(timeit.timeit(func1, number=100))
+print(timeit.timeit(lambda: func2(randrange(100), randrange(100)), number=100))
+
+
+def call_func2():
+    a = randrange(100)
+    b = randrange(100)
+    func2(a + a, a - b)
+
+
+print(timeit.timeit(call_func2, number=100))
+
+print(timeit.repeat(call_func2, repeat=10, number=100))
+
+filepath = Path(__file__).parent.joinpath("main.py")
+print(timeit.timeit(f"", "import hello_world", number=100))
+print(timeit.timeit(f"hello_world.hello()", "import hello_world", number=1))
+print(timeit.timeit(f"import hello_world", number=1))
+
+elapsed_time_second = timeit.timeit(
+    f"hello_world.hello()", "import hello_world", number=1
+)
+elapsed_time_ms = elapsed_time_second * 1000
+elapsed_time_micros = elapsed_time_ms * 1000
+elapsed_time_ns = elapsed_time_micros * 1000
+print(
+    f"{format(elapsed_time_second,'.3f')} sec, "
+    f"{format(elapsed_time_ms, '.3f')} ms, "
+    f"{format(elapsed_time_micros,'.3f')} micro sec, "
+    f"{format(elapsed_time_ns,'.3f')} ns"
+)
