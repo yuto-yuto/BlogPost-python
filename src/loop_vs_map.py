d856a845fc4ffc09c923f8b20b57dabbbbc0ee76
yuto-yuto <yuto.nakatsu@gmail.com>
2024-01-05 13:58:14 +0000

    update loop map

--- src/loop_vs_map.py
+++ src/loop_vs_map.py
@@ -1,9 +1,30 @@
 import timeit
 from typing import Dict
 from types import SimpleNamespace
-from functools import partial
 
 COUNT = 100000
+HUNDRED = 100
+
+
+def loop1():
+    result = 0
+    for i in range(HUNDRED):
+        for j in range(HUNDRED):
+            result += 1
+
+    return result
+
+
+def map1():
+    result = [0]
+
+    def run_loop():
+        for j in range(HUNDRED):
+            result[0] += 1
+
+    list(map(lambda _: run_loop(), range(HUNDRED)))
+
+    return result[0]
 
 
 def loop_append():
@@ -22,7 +43,7 @@ def map_append():
         if value % 3 == 0:
             result.append(value)
 
-    map(handle, range(1, COUNT))
+    list(map(handle, range(1, COUNT)))
 
     return len(result)
 
@@ -51,11 +72,22 @@ def map_item(data: Dict[str, str]):
         if value is not None:
             setattr(result, key, value)
 
-    map(handle, range(COUNT * 5))
+    list(map(handle, range(COUNT * 5)))
     return result
 
 
-print(timeit.timeit(loop_append, number=100))
-print(timeit.timeit(map_append, number=100))
-print(timeit.timeit(lambda: loop_item(_generate_dict()), number=10))
-print(timeit.timeit(lambda: map_item(_generate_dict()), number=10))
+print(timeit.timeit(loop1, number=100))
+print(timeit.timeit(map1, number=100))
+# print(timeit.timeit(loop_append, number=100))
+# print(timeit.timeit(map_append, number=100))
+# print(timeit.timeit(lambda: loop_item(_generate_dict()), number=10))
+# print(timeit.timeit(lambda: map_item(_generate_dict()), number=10))
+
+# print(loop1())
+# print(map1())
+# print(loop_append())
+# print(map_append())
+# result = loop_item(_generate_dict())
+# print(len(result.__dict__))
+# result = map_item(_generate_dict())
+# print(len(result.__dict__))
